e9db18946ea20f77729f6fd920154519d3f99199
praskavi <64342814+praskavi@users.noreply.github.com>
2024-06-28 09:00:53 +1000

    280624
    first commit to main

--- force-app/main/default/lwc/knowledgeArticles/knowledgeArticles.js
+++ force-app/main/default/lwc/knowledgeArticles/knowledgeArticles.js
@@ -1,7 +1,16 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
 import getArticlesOwnedByUser from '@salesforce/apex/KnowledgeArticleController.getArticlesOwnedByUser';
 
+const columns = [
+   // { label: 'Article Id', fieldName: 'Id' },
+    { label: 'Title', fieldName: 'Title' },
+    { label: 'Next Review Date', fieldName: 'NextReviewDate', type: 'date'},
+    {label: 'URL',fieldName: 'UrlName',type:'url'}
+];
+
 export default class KnowledgeArticles extends LightningElement {
+    @track columns = columns;
+    
     @wire(getArticlesOwnedByUser)
     articles;
 }
