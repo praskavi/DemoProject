298bc461b91e881b7f9748c60d46fe7622e2cd5d
praskavi <64342814+praskavi@users.noreply.github.com>
2024-06-28 11:28:58 +1000

    changes to  html and js

--- force-app/main/default/lwc/knowledgeArticles/knowledgeArticles.js
+++ force-app/main/default/lwc/knowledgeArticles/knowledgeArticles.js
@@ -2,10 +2,20 @@ import { LightningElement, wire, track } from 'lwc';
 import getArticlesOwnedByUser from '@salesforce/apex/KnowledgeArticleController.getArticlesOwnedByUser';
 
 const columns = [
-   // { label: 'Article Id', fieldName: 'Id' },
+    { label: 'Article Id', fieldName: 'Id' },
     { label: 'Title', fieldName: 'Title' },
-    { label: 'Next Review Date', fieldName: 'NextReviewDate', type: 'date'},
-    {label: 'URL',fieldName: 'UrlName',type:'url'}
+    { label: 'Next Review Date', fieldName: 'NextReviewDate', type: 'date' },
+    {
+        label: 'URL',
+        fieldName: 'articleUrl',
+        type: 'url',
+        typeAttributes: {
+            label: {
+                fieldName: 'UrlName'
+            },
+            target: '_blank'
+        }
+    }
 ];
 
 export default class KnowledgeArticles extends LightningElement {
@@ -13,4 +23,13 @@ export default class KnowledgeArticles extends LightningElement {
     
     @wire(getArticlesOwnedByUser)
     articles;
+
+    get articlesData() {
+        return this.articles.data ? this.articles.data.map(article => {
+            return {
+                ...article,
+                articleUrl: `/lightning/r/Knowledge__kav/${article.Id}/view`
+            };
+        }) : [];
+    }
 }
